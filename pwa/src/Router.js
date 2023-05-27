6a46954672d980e3630515233f9f9dbd04cb8969
HongTaeHoon <kdn95374888@gmail.com>
2023-05-28 03:03:11 +0900

    💄 모아보기 페이지 레이아웃 작업중

--- pwa/src/Router.js
+++ pwa/src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Splash from "./pages/Splash/Splash";
 import Login from "./pages/Account/Login";
 import Signup from "./pages/Account/Signup";
+import Feed from "./pages/Feed/Feed";
 import Sample from "./state/Sample";
 
 const Router = () => {
@@ -12,6 +13,7 @@ const Router = () => {
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/feed" element={<Feed />} />
         <Route path="*" element={<h1>해당 주소는 없는 페이지입니다.</h1>} />
         {/* zustand sample page */}
         <Route path="/sample" element={<Sample />} />
