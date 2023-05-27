08cff623a091c8049c95939e057a7500e62fba12
HongTaeHoon <kdn95374888@gmail.com>
2023-05-28 03:23:27 +0900

    Revert "💄 모아보기 페이지 레이아웃 작업중"
    This reverts commit 6a46954672d980e3630515233f9f9dbd04cb8969.

--- pwa/src/Router.js
+++ pwa/src/Router.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Splash from "./pages/Splash/Splash";
 import Login from "./pages/Account/Login";
 import Signup from "./pages/Account/Signup";
-import Feed from "./pages/Feed/Feed";
 import Sample from "./state/Sample";
 
 const Router = () => {
@@ -13,7 +12,6 @@ const Router = () => {
         <Route path="/" element={<Splash />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/feed" element={<Feed />} />
         <Route path="*" element={<h1>해당 주소는 없는 페이지입니다.</h1>} />
         {/* zustand sample page */}
         <Route path="/sample" element={<Sample />} />
