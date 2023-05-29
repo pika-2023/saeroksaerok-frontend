4d824e6ee991612f1322cf1549a656146ceb42bd
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-05-30 02:39:26 +0900

    :lipstick: Signup 와이어프레임 1차 완성

--- pwa/src/pages/Account/Signup.js
+++ pwa/src/pages/Account/Signup.js
@@ -1,12 +1,86 @@
 import React from "react";
 import styled from "styled-components";
+import variables from "../../styles/variables";
 
 const Signup = () => {
-  return <Title>회원가입 화면입니다</Title>;
+  return (
+    <SignupContainer>
+      <UploadProfileImg>프로필</UploadProfileImg>
+      <EditImgIcon
+        src="/icons/profileImgIcon.png"
+        alt="프로필 사진 등록하기 버튼"
+      />
+      <SignupForm>
+        <FormContainer>
+          <FormTitle>이름</FormTitle>
+          <FormInput type="text" placeholder="이름 입력란" />
+        </FormContainer>
+        <FormContainer>
+          <FormTitle>비밀번호</FormTitle>
+          <FormInput type="password" placeholder="비밀번호 입력란" />
+          <FormInput type="password" placeholder="비밀번호 확인란" />
+        </FormContainer>
+      </SignupForm>
+      <SignupButton>회원가입</SignupButton>
+    </SignupContainer>
+  );
 };
 
 export default Signup;
 
-const Title = styled.h1`
-  font-size: 2rem;
+const SignupContainer = styled.div`
+  ${variables.flex("column", "center", "center")}
+  padding-top: 25%;
+`;
+
+const UploadProfileImg = styled.div`
+  ${variables.flex("row", "center", "center")}
+  ${variables.widthHeight("147px", "147px")}
+  ${variables.fontStyle("22px", 500)}
+  margin-bottom: 50px;
+  background: ${({ theme }) => theme.style.lightgray};
+  color: ${({ theme }) => theme.style.gray};
+  border-radius: 50%;
+`;
+
+const EditImgIcon = styled.img`
+  ${variables.widthHeight("50px", "50px")}
+  margin: -100px -100px 60px 0;
+`;
+
+const SignupForm = styled.div`
+  width: 90%;
+`;
+
+const FormContainer = styled.div`
+  margin-bottom: 20px;
+`;
+
+const FormTitle = styled.div`
+  ${variables.fontStyle("24px", 500)}
+  margin-bottom: 15px;
+`;
+
+const FormInput = styled.input`
+  ${variables.fontStyle("22px", 500)}
+  margin-bottom: 20px;
+  padding: 13px 16px;
+  width: 100%;
+  background: ${({ theme }) => theme.style.lightgray};
+  border: none;
+  border-radius: 6px;
+
+  &::placeholder {
+    color: ${({ theme }) => theme.style.gray};
+  }
+`;
+
+const SignupButton = styled.button`
+  ${variables.position("fixed", "null", "null", "0", "0")}
+  ${variables.widthHeight("100%", "82px")}
+  ${variables.fontStyle("22px", 600)}
+  background: ${({ theme }) => theme.style.black};
+  color: ${({ theme }) => theme.style.white};
+  border: none;
+  cursor: pointer;
 `;
