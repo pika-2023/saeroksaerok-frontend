e3f2db59f9253dc8c3ff465ec193b6bb974ddae7
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-05-30 02:29:49 +0900

    :lipstick: Login 와이어프레임 1차 완성

--- pwa/src/pages/Account/Login.js
+++ pwa/src/pages/Account/Login.js
@@ -6,6 +6,10 @@ const Login = () => {
   return (
     <LoginContainer>
       <UploadProfileImg>프로필</UploadProfileImg>
+      <EditImgIcon
+        src="/icons/profileImgIcon.png"
+        alt="프로필 사진 수정하기 버튼"
+      />
       <LoginForm>
         <FormContainer>
           <FormTitle>이름</FormTitle>
@@ -25,15 +29,22 @@ export default Login;
 
 const LoginContainer = styled.div`
   ${variables.flex("column", "center", "center")}
+  padding-top: 25%;
 `;
 
 const UploadProfileImg = styled.div`
   ${variables.flex("row", "center", "center")}
   ${variables.widthHeight("147px", "147px")}
   ${variables.fontStyle("22px", 500)}
+  margin-bottom: 50px;
   background: ${({ theme }) => theme.style.lightgray};
   color: ${({ theme }) => theme.style.gray};
   border-radius: 50%;
+`;
+
+const EditImgIcon = styled.img`
+  ${variables.widthHeight("50px", "50px")}
+  margin: -100px -100px 60px 0;
 `;
 
 const LoginForm = styled.div`
@@ -46,11 +57,12 @@ const FormContainer = styled.div`
 
 const FormTitle = styled.div`
   ${variables.fontStyle("24px", 500)}
-  margin-bottom: 10px;
+  margin-bottom: 15px;
 `;
 
 const FormInput = styled.input`
   ${variables.fontStyle("22px", 500)}
+  margin-bottom: 20px;
   padding: 13px 16px;
   width: 100%;
   background: ${({ theme }) => theme.style.lightgray};
