a935f3439bf231021a3d4d2b1392eed5d7cc32aa
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-06-05 01:47:41 +0900

    :lipstick: Word 페이지 및 모달 레이아웃 구현

--- pwa/src/components/Modal/Modal.style.js
+++ pwa/src/components/Modal/Modal.style.js
@@ -5,67 +5,70 @@ export const RecordModal = styled.div`
   ${variables.position("fixed", "0", "0", "null", "null")}
   ${variables.flex("row", "center", "center")}
   ${variables.widthHeight("100%", "100%")}
+  ${variables.absoluteCenter}
   white-space: nowrap;
+  z-index: 100;
 `;
 
 export const ModalBackground = styled.div`
   ${variables.position("absolute", "0", "null", "null", "0")}
   ${variables.widthHeight("100vw", "100vh")}
-  background-color: rgba(0, 0, 0, 0.4);
+  background: rgba(255, 255, 255, 0.6);
+  backdrop-filter: blur(45px);
   z-index: -1;
 `;
 
 export const ModalContents = styled.div`
-  ${variables.position("absolute", "null", "null", "85px", "null")}
-  ${variables.widthHeight("100%", "259px")}
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-columns: repeat(2, 1fr);
-  padding: 15px 40px;
+  ${variables.position("absolute", "null", "null", "100px", "null")}
+  ${variables.widthHeight("100%", "259px")};
+  padding: 15px 20px;
   background: ${({ theme }) => theme.style.white};
+  border: 1px solid #e9e9e9;
   border-radius: 24px;
+  box-shadow: 0px 8px 100px rgba(47, 47, 47, 0.08);
 
   @media (min-width: 769px) {
-    ${variables.position("fixed", "null", "null", "null", "5%")}
+    ${variables.position("absolute", "null", "null", "85px", "null")}
   }
 `;
 
 export const ModalTitle = styled.h1`
   ${variables.fontStyle("24px", 600)}
-  margin: 20px 0 40px 0;
+  margin-top: 20px;
   line-height: 33px;
 `;
 
-export const ModalOptionIcon = styled.image`
-  grid-row: 2/3;
-  cursor: pointer;
+export const ModalOptionContainer = styled.div`
+  ${variables.flex("row", "center", "center")}
+  height: 107px;
 `;
 
 export const ModalOption = styled.div`
-  ${variables.flex("column", "space-between", "center")}
-  ${variables.widthHeight("142px", "100%")}
-  grid-row: 3/3;
-  margin: 30px 0;
-  gap: 20px;
+  ${variables.flex("column", "center", "center")}
+  ${variables.widthHeight("100%", "100%")}
+  margin-top: 40px;
+  background-color: ${(props) => (props.value === 0 ? "#f9f9f9" : "#fff4cc")};
+  border-radius: 12px;
+  cursor: pointer;
+`;
+
+export const ModalOptionIcon = styled.img`
+  ${variables.widthHeight("36px", "36px")}
   cursor: pointer;
 `;
 
 export const ModalOptionTitle = styled.h2`
-  ${variables.fontStyle("22px", 500)}
+  ${variables.fontStyle("19px", 600)}
   background: none;
   border: none;
-`;
-
-export const ModalButtonBox = styled.div`
-  ${variables.flex("row", "center", "center")}
-  gap: 10px;
 `;
 
 export const ModalCancelButton = styled.button`
-  ${variables.position("fixed", "null", "null", "32px", "null")}
+  ${variables.position("fixed", "null", "40%", "40px", "null")}
   ${variables.fontStyle("19px", 500)}
-  color: ${({ theme }) => theme.style.white};
   background: none;
   border: none;
+  z-index: 100;
+  opacity: 40%;
   cursor: pointer;
 `;
