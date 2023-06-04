962c503a1bc2efb4e4909e97e2745c18c729a89d
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-06-05 04:49:22 +0900

    :art: voiceRecord 구조 개선

--- pwa/src/pages/Word/VoiceRecord/VoiceRecord.style.js
+++ pwa/src/pages/Word/VoiceRecord/VoiceRecord.style.js
@@ -3,31 +3,24 @@ import variables from "../../../styles/variables";
 
 export const VoiceRecordContainer = styled.div`
   ${variables.widthHeight("100vw", "100vh")}
+  position: relative;
   margin: calc(-20px);
-  padding-top: 120px;
-`;
-
-export const VoiceRecorder = styled.div`
-  ${variables.widthHeight("100%", "300px")}
-  display: grid;
-  grid-template-rows: repeat(8, 1fr);
-  margin: calc(-20px);
-  background: url("./images/bg_reminisce_before.png");
+  background: url("./images/bg_reminisce.png") no-repeat;
   background-size: cover;
+  overflow: hidden;
 
   @media (min-width: 769px) {
     ${variables.widthHeight("375px", "685px")}
-    margin: calc(-20px);
-    background: url("./images/bg_reminisce_before.png");
-    background-size: cover;
   }
 `;
 
 export const VoiceRecordTitle = styled.h2`
   ${variables.fontStyle("32px", 600)}
-  grid-row: 4;
+  ${variables.position("absolute", "42%", "null", "null", "50%")}
   text-align: center;
   letter-spacing: -0.03em;
+  white-space: nowrap;
+  transform: translate(-50%, -50%);
 `;
 
 // MARK: fade in-out animation css
@@ -44,29 +37,32 @@ const fadeIn = keyframes`
 `;
 
 export const ListeningText = styled.h6`
+  ${variables.position("absolute", "70%", "null", "null", "50%")}
   ${variables.fontStyle("24px", 500)}
-  grid-row: 5;
   color: ${({ theme }) => theme.style.gray5};
   line-height: 35px;
   text-align: center;
   letter-spacing: -0.03em;
+  transform: translate(-50%, -50%);
   animation: ${fadeIn} 3s infinite;
   ${({ isVisible }) => isVisible && "opacity: 1;"}
+  white-space: nowrap;
   opacity: 0;
 `;
 
 export const StartRecordButton = styled.button`
+  ${variables.position("absolute", "null", "null", "10%", "50%")}
   ${variables.flex("row", "center", "center")}
-  ${variables.widthHeight("100%", "68px")}
+  ${variables.widthHeight("90%", "68px")}
   ${variables.fontStyle("22px", 600)}
-  grid-row: 7;
   background: ${({ theme }) => theme.style.yellow1};
   color: ${({ theme }) => theme.style.gray5};
   line-height: 32px;
   text-align: center;
   letter-spacing: -0.03em;
   border: none;
-  border-radius: 24px;
+  border-radius: 20px;
+  transform: translate(-50%, -50%);
   cursor: pointer;
 `;
 
@@ -78,11 +74,12 @@ export const StartRecordButton = styled.button`
 // `;
 
 export const CancelButton = styled.button`
+  ${variables.position("absolute", "null", "null", "5%", "50%")}
   ${variables.flex("row", "center", "center")}
   ${variables.fontStyle("19px", 500)}
-  grid-row: 8;
   color: ${({ theme }) => theme.style.gray3};
   background: none;
   border: none;
+  transform: translate(-50%, -50%);
   cursor: pointer;
 `;
