5795a3559ac824997df0d5f43c245a39c96a23a5
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-06-10 10:47:36 +0900

    :adhesive_bandage: simple fix

--- pwa/src/pages/Word/VoiceRecord/RcFirst.js
+++ pwa/src/pages/Word/VoiceRecord/RcFirst.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import useStore from "../../../state/store";
 import MicRecorder from "mic-recorder-to-mp3";
 import * as S from "../Word.style";
 import * as W from "./VoiceRecord.style";
+import Recall from "../../../components/Recall/Recall";
 
 const RcFirst = (e) => {
   const navigate = useNavigate();
@@ -16,6 +17,8 @@ const RcFirst = (e) => {
     setIsRecording,
     recorder,
     setRecorder,
+    isSplashOpen,
+    setIsSplashOpen,
   } = useStore((state) => state);
 
   const startRecording = async () => {
@@ -67,35 +70,44 @@ const RcFirst = (e) => {
     }
   };
 
+  useEffect(() => {
+    setTimeout(function () {
+      setIsSplashOpen(false);
+    }, 2000);
+  }, []);
+
   return (
-    <W.VoiceRecordContainer>
-      <S.TodayWordContainer>
-        <S.TodayWord>
-          {(function () {
-            if (keyword === "가족")
-              return <S.TodayWordContent src="/images/text_family.png" />;
-            if (keyword === "친구")
-              return <S.TodayWordContent src="/images/text_friend.png" />;
-            if (keyword === "여행")
-              return <S.TodayWordContent src="/icons/text_travel.png" />;
-          })()}
-        </S.TodayWord>
-      </S.TodayWordContainer>
-      <W.QuestionText>언제 있었던 일인가요?</W.QuestionText>
-      {isRecording ? (
-        <>
-          <W.ListeningText>귀기울여 듣고 있어요</W.ListeningText>
-          <W.RecordBtn onClick={handleButtonClick}>말 끝내기</W.RecordBtn>
-        </>
-      ) : (
-        <W.RecordBtn onClick={handleButtonClick} value={"start"}>
-          말 시작하기
-        </W.RecordBtn>
-      )}
-      <W.CancelBtn onClick={() => navigate("/word")} value={"start"}>
-        취소하기
-      </W.CancelBtn>
-    </W.VoiceRecordContainer>
+    <>
+      {isSplashOpen && <Recall />}
+      <W.VoiceRecordContainer>
+        <S.TodayWordContainer>
+          <S.TodayWord>
+            {(function () {
+              if (keyword === "가족")
+                return <S.TodayWordContent src="/images/text_family.png" />;
+              if (keyword === "친구")
+                return <S.TodayWordContent src="/images/text_friend.png" />;
+              if (keyword === "여행")
+                return <S.TodayWordContent src="/icons/text_travel.png" />;
+            })()}
+          </S.TodayWord>
+        </S.TodayWordContainer>
+        <W.QuestionText>언제 있었던 일인가요?</W.QuestionText>
+        {isRecording ? (
+          <>
+            <W.ListeningText>귀기울여 듣고 있어요</W.ListeningText>
+            <W.RecordBtn onClick={handleButtonClick}>말 끝내기</W.RecordBtn>
+          </>
+        ) : (
+          <W.RecordBtn onClick={handleButtonClick} value={"start"}>
+            말 시작하기
+          </W.RecordBtn>
+        )}
+        <W.CancelBtn onClick={() => navigate("/word")} value={"start"}>
+          취소하기
+        </W.CancelBtn>
+      </W.VoiceRecordContainer>
+    </>
   );
 };
 
