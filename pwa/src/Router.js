de4c4291724429e4fbda1065c2e44bb106e7e559
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-06-10 18:17:03 +0900

    :coffin: VoiceRecord 삭제

--- pwa/src/Router.js
+++ pwa/src/Router.js
@@ -5,7 +5,6 @@ import Signup from "./pages/Account/Signup";
 import Feed from "./pages/Feed/Feed";
 import FeedDetail from "./pages/Feed/FeedDetail";
 import Word from "./pages/Word/Word";
-import VoiceRecord from "./pages/Word/VoiceRecord/VoiceRecord";
 import TextResult from "./pages/Word/TextResult/TextResult";
 import PictureResult from "./pages/Word/PictureResult/PictureResult";
 import Sample from "./state/Sample";
@@ -23,7 +22,6 @@ const Router = () => {
         <Route path="/feed" element={<Feed />} />
         <Route path="/feedDetail" element={<FeedDetail />} />
         <Route path="/word" element={<Word />} />
-        <Route path="/voiceRecord" element={<VoiceRecord />} />
         <Route path="/question-1" element={<RcFirst />} />
         <Route path="/question-2" element={<RcSecond />} />
         <Route path="/question-3" element={<RcThird />} />
