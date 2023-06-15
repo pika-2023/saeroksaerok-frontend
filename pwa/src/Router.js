f8c567bf3ac69b4a6e95781704aa89f60ec2780d
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-06-15 18:21:17 +0900

    :coffin: zustand sample 페이지 삭제

--- pwa/src/Router.js
+++ pwa/src/Router.js
@@ -2,30 +2,27 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Account/Login";
 import Signup from "./pages/Account/Signup";
-import Feed from "./pages/Feed/Feed";
-import FeedDetail from "./pages/Feed/FeedDetail/FeedDetail";
 import Word from "./pages/Word/Word";
+import VoiceRecord from "./pages/Word/VoiceRecord/VoiceRecord";
 import TextResult from "./pages/Word/TextResult/TextResult";
 import PictureResult from "./pages/Word/PictureResult/PictureResult";
-import Sample from "./state/Sample";
-import VoiceRecord from "./pages/Word/VoiceRecord/VoiceRecord";
+import Feed from "./pages/Feed/Feed";
+import FeedDetail from "./pages/Feed/FeedDetail/FeedDetail";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
         {/* MARK: 시연을 위해 /signup을 main url로 설정 */}
         <Route path="/" element={<Signup />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/feedDetail" element={<FeedDetail />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/word" element={<Word />} />
         <Route path="/voiceRecord" element={<VoiceRecord />} />
         <Route path="/textResult" element={<TextResult />} />
         <Route path="/pictureResult" element={<PictureResult />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/feedDetail" element={<FeedDetail />} />
         <Route path="*" element={<h1>해당 주소는 없는 페이지입니다.</h1>} />
-        {/* zustand sample page */}
-        <Route path="/sample" element={<Sample />} />
       </Routes>
     </BrowserRouter>
   );
